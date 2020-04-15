5d2824b0d6476746745ae9326301fea082d509f7
Justin Yap <justinyapcc@gmail.com>
2020-04-15 17:24:04 +1000

    Update documentation

--- R/properties.R
+++ R/properties.R
@@ -139,10 +139,10 @@ addQuotesOrComma <- function(v, term)
 #' Copy attributes from one object to another
 #'
 #' Copies attributes such as "label", "name", "question" and "questiontype"
-#' from one object to another. If both objects are lists [data frames],
-#' elements [columns] in the recipient list [data frame] will also receive
-#' attributes from elements [columns] with the same name in the donor list
-#' [data frame]. The function is recursive, and will copy attributes when the
+#' from one object to another. If both objects are lists (data frames),
+#' elements (columns) in the recipient list (data frame) will also receive
+#' attributes from elements (columns) with the same name in the donor list
+#' (data frame). The function is recursive, and will copy attributes when the
 #' inputs are nested lists.
 #' @param data.without.attributes an object to receive attributes from, such as
 #' a data.frame, list, or matrix
