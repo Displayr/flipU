beb5739a7530cb51ec8d5df1ca3107ee740749ed
Bruce Tan <bruce.tan@displayr.com>
2025-02-25 16:45:08 +1300

    Untracked: pack flipU in Nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Nix flake to build and export the flipU R Package";
+
+  inputs = {
+    # requires you setup your ssh keys with github https://github.com/settings/keys
+    nixr.url = "git+ssh://git@github.com/Displayr/NixR.git";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixr, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixr.pkgs.${system};
+        extractRVersion = nixr.extractRVersion;
+
+        descriptionFile = builtins.readFile ./DESCRIPTION;
+        packageVersion = extractRVersion descriptionFile;
+      in {
+        packages.flipU = pkgs.callPackage ./default.nix { 
+          inherit pkgs packageVersion;
+        };
+        defaultPackage = self.packages.${system}.flipU;
+      }
+    );
+}
