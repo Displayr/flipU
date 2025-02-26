9c27ed6803dc45304ab5c678efa1042f642147d7
Bruce Tan <bruce.tan@displayr.com>
2025-02-26 18:28:51 +1300

    DPI-2830: Pack flipU in Nix

---
+++ package.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} , displayrUtils }:
+
+pkgs.rPackages.buildRPackage {
+  name = "flipU";
+  version = displayrUtils.extractRVersion (builtins.readFile ./DESCRIPTION); 
+  src = ./.;
+  description = "Utility functions used across multiple flip packages.";
+  propagatedBuildInputs = with pkgs.rPackages; [ pkgs.R R_utils ];
+}
