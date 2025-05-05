38bbefb095dc7538c62c0bd4c50fb76b6ea03d6b
Jordan Cran <jordancran@hey.com>
2025-05-05 14:03:27 +1000

    DPI-2964: use nix CI

--- package.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {}, displayrUtils }:
-
-pkgs.rPackages.buildRPackage {
-  name = "flipU";
-  version = displayrUtils.extractRVersion (builtins.readFile ./DESCRIPTION); 
-  src = ./.;
-  description = "Utility functions used across multiple flip packages.";
-  propagatedBuildInputs = with pkgs.rPackages; [ R_utils ];
-}
